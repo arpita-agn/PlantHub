cdcee8fc458a9a472d02cc8602cc8781993cc77d
agn2017331008 <ghosharpita423@gmail.com>
2024-05-12 10:16:40 +0600

    Project Initialization

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
+
+const ProductDisplay = (props) => {
+  const { product } = props;
+  const { addToCart } = useContext(ShopContext);
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>122</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+          debitis doloremque quis minus perferendis, ipsum nam nobis et vitae
+          nemo aspernatur labore consequuntur consequatur voluptate neque
+          excepturi fuga obcaecati distinctio.
+        </div>
+        <button onClick={()=>addToCart(product.id)}>Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDisplay;
